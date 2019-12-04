66bdc03cbc0974181eb2f0e895515ec1c638fbd7
m5r <mokht@rmi.al>
2019-12-04 16:38:11 +0100

    clarify conditions to reply to status

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,11 @@ logger.log("waiting for new tweets from ", NARAKU_TWITTER_ID);
 stream.on("tweet", (tweet: Twit.Twitter.Status) => {
 	logger.log("new tweet %s from %s", tweet.id_str, tweet.user.id_str);
 
-	// TODO: vérifier que c'est pas un RT
-	// TODO: vérifier que l'autheur === Naraku
-	if (tweet.in_reply_to_status_id === null) {
+	const isStandaloneStatus = tweet.in_reply_to_status_id === null;
+	const isStatusFromNaraku = tweet.user.id_str === NARAKU_TWITTER_ID;
+	const isNotRetweet = !tweet.hasOwnProperty("retweeted_status");
+	const shouldReplyToStatus = isStandaloneStatus && isStatusFromNaraku && isNotRetweet;
+	if (shouldReplyToStatus) {
 		logger.log("replying to", tweet.id_str);
 
 		bot.tweet(buildTweet(), tweet.id_str);
