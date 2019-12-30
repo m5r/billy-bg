e1fe522172df334d1682a0464864cc112849e93c
m5r <mokht@rmi.al>
2019-12-30 05:23:33 +0100

    reply to status with @mention

--- src/index.ts
+++ src/index.ts
@@ -15,17 +15,17 @@ const watchedUser = process.env.TWITTER_USER_ID ?? NARAKU_TWITTER_ID;
 const stream = bot.watchUserTweets(watchedUser);
 logger.log("waiting for new tweets from %s", watchedUser);
 
-stream.on("tweet", (tweet: Twit.Twitter.Status) => {
+stream.on("tweet", async (tweet: Twit.Twitter.Status) => {
 	logger.log("new tweet %s from %s", tweet.id_str, tweet.user.id_str);
 
-	const isStandaloneStatus = tweet.in_reply_to_status_id === null;
-	const isStatusFromNaraku = tweet.user.id_str === watchedUser;
+	const isStandaloneStatus = !tweet.hasOwnProperty("in_reply_to_status_id") || tweet.in_reply_to_status_id === null;
+	const isStatusFromWatchedUser = tweet.user.id_str === watchedUser;
 	const isNotRetweet = !tweet.hasOwnProperty("retweeted_status");
-	const shouldReplyToStatus = isStandaloneStatus && isStatusFromNaraku && isNotRetweet;
+	const shouldReplyToStatus = isStandaloneStatus && isStatusFromWatchedUser && isNotRetweet;
 	if (shouldReplyToStatus) {
 		logger.log("replying to", tweet.id_str);
 
-		bot.tweet(buildTweet(), tweet.id_str);
+		bot.tweet(`@${tweet.user.screen_name} ${buildTweet()}`, tweet.id_str);
 	}
 });
 
