58536396db6a7422bc8e45e40d06bdb0f8597c36
m5r <mokht@rmi.al>
2019-12-30 05:33:04 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@
 sed -i '/.env/d' .gitignore
 git add .
 git commit -m "push to dokku with .env"
-git push dokku master
-git reset --hard HEAD^
+git push dokku master -f
+git reset --soft HEAD^
+git checkout origin/master .gitignore
+git reset HEAD .env
